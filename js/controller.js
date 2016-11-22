e4f28c8d47e958806fe549cde6b050cf8583c8c3
Brendan Greene <brendanjgreene@gmail.com>
2016-11-22 17:54:09 +0000

    modify our RegisterController so we can use it to store our token

--- js/controller.js
+++ js/controller.js
@@ -7,16 +7,11 @@ angular.module('RouteControllers', [])
         $scope.registrationUser = {};
         var URL = "https://morning-castle-91468.herokuapp.com/";
 
-        var authStorage = {
-            name: "StorageTest"
-        };
- 
-        store.set('obj', authStorage);
-
         $scope.login = function() {
             UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
                 $scope.token = results.data.token;
-                console.log($scope.token);
+                store.set('username', $scope.registrationUser.username);
+                store.set('authToken', $scope.token);
             }).catch(function(err) {
                 console.log(err.data);
             });
