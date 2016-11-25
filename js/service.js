6183a6bafc86c5bfd1fb33c3aa7e0d97660fa784
Brendan Greene <brendanjgreene@gmail.com>
2016-11-25 16:11:30 +0000

    semicolons added

--- js/service.js
+++ js/service.js
@@ -2,11 +2,11 @@ angular.module('UserService', [])
 	.factory('UserAPIService', function($http) {
 		UserAPIService = {
 			callAPI: function(url, data) {
-				return $http.post(url, data)
+				return $http.post(url, data);
 			}
-		}
+		};
 		return UserAPIService;
-	})
+	});
 
 angular.module('TodoService', [])
     .factory('TodoAPIService', function($http) {
